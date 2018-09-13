779883904b803791efc4149e7486391405904089
Chanyub Park <mrchypark@gmail.com>
2018-09-13 17:09:35 +0900

    init

---
+++ source.R
@@ -0,0 +1,26 @@
+install.packages("devtools")
+library(devtools)
+install.packages("installr", type = "binary")
+library(installr)
+install.rtools()
+Sys.setenv(TAR = "C:/Rtools/bin/tar.exe")
+
+install_github("forkonlp/presidentSpeechKr")
+install_github("talgalili/installr")
+library(installr)
+
+install.java()
+install.packages("KoNLP")
+library(KoNLP)
+
+install.packages("tidytext")
+
+install_github("mrchypark/RcppMecabe")
+installMecabKoForWindows()
+installMecabKoDicForWindlows()
+
+test <- "한글 테스트 입니다."
+test <- iconv(test, to = "UTF-8")
+pos(test)
+
+extractNoun(test)
