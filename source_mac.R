91e6d0170b25ace19b07a7c440cc46d5614b7d98
Chanyub.Park <mrchypark@gmail.com>
2018-09-17 14:43:10 +0900

    for pull

--- source_mac.R
+++ source_mac.R
@@ -51,6 +51,8 @@ install_github('haven-jeon/KoSpacing')
 library(KoSpacing)
 spacing("김형호영화시장분석가는'1987'의네이버영화정보네티즌10점평에서언급된단어들을지난해12월27일부터올해1월10일까지통계프로그램R과KoNLP패키지로텍스트마이닝하여분석했다.")
 
+
+
 install.packages("tidytext")
 install_github("forkonlp/presidentSpeechKr")
 
